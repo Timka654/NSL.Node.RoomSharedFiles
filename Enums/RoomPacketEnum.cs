39e94d9d3e0a0b14fb845caa579612a567acb750
Timur Meyliev <Timka654work@gmail.com>
2023-02-13 17:56:00 +0200

    move packet enum for client-room connection

---
+++ Enums/RoomPacketEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSL.Node.RoomServer.Shared.Enums
+{
+    public enum RoomPacketEnum
+    {
+        SignSession = 1,
+        SignSessionResult = SignSession,
+        ChangeNodeList,
+        Transport,
+        Broadcast,
+        ReadyNode,
+        ReadyNodeResult = ReadyNode,
+        ReadyRoom,
+        Execute
+    }
+}
