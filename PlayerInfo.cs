906052c9e6c65e26c2d546ae89eaeaef5545fc78
Timur Meyliev <Timka654work@gmail.com>
2023-03-03 01:03:13 +0200

    fix

--- PlayerInfo.cs
+++ PlayerInfo.cs
@@ -5,8 +5,19 @@ namespace NSL.Node.RoomServer.Shared.Client.Core
 {
     public class PlayerInfo
     {
-        public IPlayerNetwork Network { get; set; }
+        public IPlayerNetwork Network { get; }
 
-        public Guid Id { get; set; }
+        public Guid Id { get; }
+
+        ClientObjectBag ObjectBag => Network.ObjectBag;
+
+        public void InitializeObjectBag()
+            => Network.InitializeObjectBag();
+
+        public PlayerInfo(IPlayerNetwork network, Guid id)
+        {
+            Network = network;
+            Id = id;
+        }
     }
 }
