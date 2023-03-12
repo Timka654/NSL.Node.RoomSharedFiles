f832df66662c3ead52f1af1ebd1ee3e48b96eb79
Timur Meyliev <Timka654work@gmail.com>
2023-03-12 04:12:21 +0200

    Update PlayerNetwork for send with udpUpdate nuget refs in template project to latest

--- IPlayerNetwork.cs
+++ IPlayerNetwork.cs
@@ -1,5 +1,7 @@
 ﻿using NSL.SocketCore.Utils;
 using NSL.SocketCore.Utils.Buffer;
+using NSL.UDP;
+using NSL.UDP.Enums;
 using System;
 
 namespace NSL.Node.RoomServer.Shared.Client.Core
@@ -11,6 +13,13 @@ namespace NSL.Node.RoomServer.Shared.Client.Core
         void Transport(Action<OutputPacketBuffer> build);
 
         void Send(OutputPacketBuffer packet, bool disposeOnSend = true);
+
+
+        void Transport(Action<DgramPacket> build, ushort code, UDPChannelEnum channel = UDPChannelEnum.ReliableOrdered);
+
+        void Transport(Action<DgramPacket> build, UDPChannelEnum channel = UDPChannelEnum.ReliableOrdered);
+
+        void Send(DgramPacket packet, UDPChannelEnum channel = UDPChannelEnum.ReliableOrdered, bool disposeOnSend = true);
 
         ClientObjectBag ObjectBag { get; }
 
